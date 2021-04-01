d49669178760a9f9df4acb54e3a4d83ec8972bba
Alexander Denisov <pmax45487@gmail.com>
2021-04-01 17:14:19 +0400

    Insert into beginning

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
@@ -7,7 +8,32 @@ public class Main {
 
         Test [] tests = new Test[5];
 
-        ArrayList arrList = new ArrayList();
-        LinkedList linkList = new LinkedList();
+        for (int i = 0; i<5 ;i++) {
+            tests[i] = new Test();
+        }
+
+        List arrList = new ArrayList();
+        List linkList = new LinkedList();
+        long startTime, endTime;
+
+
+        tests[0].methodName = "Insert into beginning";
+        tests[0].countOfIterations = 10000;
+        startTime = System.nanoTime();
+        for (int i = 0; i<10000; i++) {
+            arrList.add(0,i);
+        }
+        endTime = System.nanoTime();
+        tests[0].timeOfExecutionArray = (endTime - startTime)/1000000;
+
+        startTime = System.nanoTime();
+        for (int i = 0; i<10000; i++) {
+            linkList.add(0,i);
+        }
+        endTime = System.nanoTime();
+        tests[0].timeOfExecutionLinked = (endTime - startTime)/1000000;
+
+        tests[0].printTest();
     }
+
 }
--- src/Test.java
+++ src/Test.java
@@ -4,11 +4,23 @@ public class Test {
 
     public String methodName;
     public int countOfIterations;
-    public long timeOfExecution;
+    public long timeOfExecutionLinked;
+    public long timeOfExecutionArray;
 
-    public Test(String methodName, int countOfIterations, long timeOfExecution) {
+    public Test(String methodName, int countOfIterations,
+                long timeOfExecutionLinked, long timeOfExecutionArray) {
         this.methodName = methodName;
         this.countOfIterations = countOfIterations;
-        this.timeOfExecution = timeOfExecution;
+        this.timeOfExecutionLinked = timeOfExecutionLinked;
+        this.timeOfExecutionArray = timeOfExecutionArray;
+    }
+
+    public Test() {
+    }
+
+    public void printTest() {
+        System.out.println("Method name:" + methodName + ", count of iterations:" +
+                countOfIterations + ", time for linked list:"
+                + timeOfExecutionLinked + ", time for array list:" + timeOfExecutionArray);
     }
 }
