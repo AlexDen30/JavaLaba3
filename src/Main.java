929cd2c7229e211bdb16d4a6213c8179e09c3ec9
Alexander Denisov <pmax45487@gmail.com>
2021-04-01 15:49:01 +0400

    Init

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Test [] tests = new Test[5];
+
+        ArrayList arrList = new ArrayList();
+        LinkedList linkList = new LinkedList();
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,14 @@
+
+
+public class Test {
+
+    public String methodName;
+    public int countOfIterations;
+    public long timeOfExecution;
+
+    public Test(String methodName, int countOfIterations, long timeOfExecution) {
+        this.methodName = methodName;
+        this.countOfIterations = countOfIterations;
+        this.timeOfExecution = timeOfExecution;
+    }
+}
